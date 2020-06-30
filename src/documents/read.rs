889b10bc05962135066efc6e27de0adedb7cf5c8
Leo Wong <leo@agel.com.hk>
2020-06-30 17:49:32 +0800

    Revert "added async read function"
    This reverts commit aee3891587d3d4f581114c5aab5120a9c9cf35fb.

--- src/documents/read.rs
+++ src/documents/read.rs
@@ -25,31 +25,6 @@ where
 }
 
 ///
-/// Read a document of a specific type from a collection by its Firestore document name
-///
-/// ## Arguments
-/// * 'auth' The authentication token
-/// * 'document_name' The document path / collection and document id; For example "projects/my_project/databases/(default)/documents/tests/test"
-pub async fn read_by_name_async<T>(auth: &impl FirebaseAuthBearer, document_name: impl AsRef<str>) -> Result<T>
-where
-    for<'b> T: Deserialize<'b>,
-{
-    let url = firebase_url_base(document_name.as_ref());
-
-    let resp = auth
-        .client_async()
-        .get(&url)
-        .bearer_auth(auth.access_token().to_owned())
-        .send()
-        .await?;
-
-    let resp = extract_google_api_error_async(resp, || document_name.as_ref().to_owned()).await?;
-
-    let json: dto::Document = resp.json().await?;
-    Ok(document_to_pod(&json)?)
-}
-
-///
 /// Read a document of a specific type from a collection
 ///
 /// ## Arguments
@@ -67,24 +42,4 @@ where
         document_id.as_ref()
     );
     read_by_name(auth, &document_name)
-}
-
-///
-/// Read a document of a specific type from a collection
-///
-/// ## Arguments
-/// * 'auth' The authentication token
-/// * 'path' The document path / collection; For example "my_collection" or "a/nested/collection"
-/// * 'document_id' The document id. Make sure that you do not include the document id to the path argument.
-pub async fn read_async<T>(auth: &impl FirebaseAuthBearer, path: &str, document_id: impl AsRef<str>) -> Result<T>
-where
-    for<'b> T: Deserialize<'b>,
-{
-    let document_name = format!(
-        "projects/{}/databases/(default)/documents/{}/{}",
-        auth.project_id(),
-        path,
-        document_id.as_ref()
-    );
-    read_by_name_async(auth, &document_name).await
 }
